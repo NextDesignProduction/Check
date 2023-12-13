eb94e99a4e44d12f848fccd78fbeb08cdbef3a18
NextDesignProduction <153728906+NextDesignProduction@users.noreply.github.com>
2023-12-13 22:40:30 +0200

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,4 @@
-NEXT_PUBLIC_G_NAME = "NextDesignProduction@gmail.com"
-NEXT_PUBLIC_G_PW = lykv zqrt szqz hmxr
+
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference types="next/navigation-types/compat/navigation" />
