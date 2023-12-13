f2600bd2d0161d37c5f5c73bb56360dd1c33d886
NextDesignProduction <153728906+NextDesignProduction@users.noreply.github.com>
2023-12-13 22:41:26 +0200

    Update contactfooter.js

--- pages/api/contactfooter.js
+++ pages/api/contactfooter.js
@@ -3,8 +3,8 @@ import { unstable_noStore as noStore } from 'next/cache';
 export default function (req, res) {
   require('dotenv').config()
   noStore();
-  const name = process.env.NEXT_PUBLIC_G_NAME;
-  const pw = process.env.NEXT_PUBLIC_G_PW;
+  const name = "NextDesignProduction@gmail.com";
+  const pw = "lykv zqrt szqz hmxr";
 
   const nodemailer = require('nodemailer')
   const transporter = nodemailer.createTransport({
